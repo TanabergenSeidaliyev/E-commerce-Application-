f1aebfb05c89e4cf39a016a5282e17f0495534ca
TanabergenSeidaliyev <seidaliyevsarsenbaiuly@gmail.com>
2024-10-31 00:21:33 +0600

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_new_project_1/view/first_screen_page.dart';
+
+void main() {
+  runApp(FirstScreenPage());
+}
+// view деген папка да страница лар болады
+// 2 стр создать етдим 1 стр 2 страница га библеотекадан пайдаланип отеди
+// flutter библеотекадан бари турады тек библеотекаларди пайдаланип билиу керек
+// библеотекалар сайти pub.dev
+// 1 стр 2 стр отив auto router деген библеотека
---
+++ lib/view/second_screen_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SecondScreenPage extends StatefulWidget {
+  const SecondScreenPage({super.key});
+
+  @override
+  State<SecondScreenPage> createState() => _SecondScreenPageState();
+}
+
+class _SecondScreenPageState extends State<SecondScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
